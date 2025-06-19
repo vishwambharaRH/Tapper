7e79ec7a98d402e43bc0c9417c118ad863ea1adc
Vishwambhara R Hebbalalu <vishwambharah@gmail.com>
2025-06-19 14:48:15 +0530

    Initial Commit

---
+++ Tapper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tapper
+//
+//  Created by Vishwambhara on 19/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TapperTests/TapperTests.swift
@@ -0,0 +1,17 @@
+//
+//  TapperTests.swift
+//  TapperTests
+//
+//  Created by Vishwambhara on 19/06/25.
+//
+
+import Testing
+@testable import Tapper
+
+struct TapperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
